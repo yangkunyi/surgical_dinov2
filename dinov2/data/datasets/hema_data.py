6b0243b3b8ea9e113a4f92e1cfd9733427b5063c
kyyang <yangkunyi@sjtu.edu.cn>
2025-05-20 21:00:19 +0800

    segmentation loss added

--- dinov2/data/datasets/hema_data.py
+++ dinov2/data/datasets/hema_data.py
@@ -11,6 +11,8 @@ import torch
 from PIL import Image
 from torchvision.datasets import VisionDataset
 import torchvision.transforms as T
+import numpy as np
+import cv2
 
 logger = logging.getLogger("dinov2")
 
@@ -56,12 +58,20 @@ class HemaStandardDataset(VisionDataset):
             print(f"can not read depth for sample {index, e, filepath}")
             return self.__getitem__(index + 1)
 
+        mask = self.get_seg_data(index)
         target = self.get_target(index)
 
         if self.transforms is not None:
             image, target = self.transforms(image, target)
 
-        return image, target, filepath, T.ToTensor()(raw_image), T.ToTensor()(depth)
+        return (
+            image,
+            target,
+            filepath,
+            T.ToTensor()(raw_image),
+            T.ToTensor()(depth),
+            torch.as_tensor(np.array(mask), dtype=torch.int8).unsqueeze(0),
+        )
 
     def get_image_data(self, index: int, dimension=224) -> Image:
         # Load image from jpeg file
@@ -91,6 +101,27 @@ class HemaStandardDataset(VisionDataset):
             .resize((dimension, dimension), Image.Resampling.LANCZOS)
         )
         return depth, depthpath
+
+    def get_seg_data(
+        self,
+        index: int,
+        dimension=224,
+        seg_base_path="/bd_byta6000i0/users/surgicaldinov2/kyyang/segmentation_on_cholec80/masks",
+    ) -> Image:
+        adjusted_index = index % self.true_len
+        filepath = self.patches[adjusted_index]
+        splited_path = filepath.split("/")
+        seg_file_name = splited_path[-2] + "_" + splited_path[-1]
+        maskpath = os.path.join(seg_base_path, seg_file_name)
+        mask = (
+            Image.open(maskpath)
+            .convert(mode="L")
+            .resize(
+                (dimension, dimension),
+                Image.Resampling.NEAREST,
+            )
+        )
+        return mask
 
     def get_target(self, index: int) -> torch.Tensor:
         # labels are not used for training
